81575e0a422e0e0fb2533501530c44e29011061c
mohamed-amr28 <mohamed.ma.amr28@gmail.com>
2026-05-02 19:12:28 +0300

    APP

---
+++ APP/main.c
@@ -0,0 +1,43 @@
+﻿#include "Joystick.h"
+#include "battery.h"
+#include "MOTOR_Interface.h"
+#include "GPIO_Interface.h"
+#include "ADC_Interface.h"
+#include "Types.h"
+
+int main(void)
+{
+    ADC_Init();
+
+    // Motor control pins
+    pinmode(GPIO_PORTB, PIN0, OUTPUT);
+    pinmode(GPIO_PORTB, PIN1, OUTPUT);
+    pinmode(GPIO_PORTB, PIN2, OUTPUT);
+    pinmode(GPIO_PORTB, PIN3, OUTPUT);
+
+    // Battery indicator LEDs
+    pinmode(GPIO_PORTD, PIN2, OUTPUT);
+    pinmode(GPIO_PORTD, PIN3, OUTPUT);
+
+    // Analog joystick / battery pins (ADC channels)
+    pinmode(GPIO_PORTC, PIN0, INPUT);
+    pinmode(GPIO_PORTC, PIN1, INPUT);
+    pinmode(GPIO_PORTC, PIN2, INPUT);
+
+    MOTOR_stop();
+
+    while (1)
+    {
+        Joystick_Read();
+        Joystick_Control();
+        battery_read();
+        battery(batt);
+
+        if (batt <= BATTERY_LOW_THRESHOLD)
+        {
+            MOTOR_stop();
+        }
+    }
+
+    return 0;
+}
